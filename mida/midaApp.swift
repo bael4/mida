0f463d487e8e1ab4d37477af9aea864a11875d70
bael4 <bayel.ryspekov@gmail.com>
2024-06-05 21:02:19 +0600

    Initial Commit

---
+++ mida/midaApp.swift
@@ -0,0 +1,17 @@
+//
+//  midaApp.swift
+//  mida
+//
+//  Created by Баэль Рыспеков on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct midaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
